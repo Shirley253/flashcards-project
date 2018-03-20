39573a2c53c92acc9c7b78f63bba7765bae6b7a8
andrew lupton and shirley dennis <shirley@>
2018-03-20 16:06:27 -0700

    add flashcards project

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#flash1").click(function() {
+    $("#question1").toggle();
+    $("#answer1").toggle();
+  });
+  $("#flash2").click(function() {
+    $("#question2").toggle();
+    $("#answer2").toggle();
+  });
+  $("#flash3").click(function() {
+      $("#question3").toggle();
+      $("#answer3").toggle();
+  });
+});
